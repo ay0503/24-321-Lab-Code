ec1f9b8440ca30ec577f9a74d82c72c9c1fa8efb
Andrew Youn <52907065+ay0503@users.noreply.github.com>
2023-04-03 14:50:41 -0400

    HT36C tables done without B data

--- HT36C/plotting_environment.py
+++ HT36C/plotting_environment.py
@@ -2,10 +2,26 @@ import pandas as pd
 import math, csv
 import matplotlib.pyplot as plt
 import numpy as np
+import inspect
 
 ###############################################################################
 # HELPER FUNCTIONS & CONSTANTS
 ###############################################################################
+
+x, y, z = 1, 2, 3
+
+def retrieve_name(var):
+    callers_local_vars = inspect.currentframe().f_back.f_back.f_locals.items()
+    return [var_name for var_name, var_val in callers_local_vars if var_val is var][0]
+
+def save(*args, length = None):
+    if length == None:
+        length = len(args[0])
+    for arg in args:
+        if not isinstance(arg, np.ndarray):
+            VALUES[retrieve_name(arg)] = np.array([arg]*length)
+        else:
+            VALUES[retrieve_name(arg)] = arg
 
 # returns the time value from a given sample number
 def sample_to_time(n: int) -> int:
@@ -25,12 +41,14 @@ def linear_interpolation(x1 : float, y1 : float, x2 : float,
 
 def find_steady_state(data : list[float]) ->int:
     assert(len(data) > 10)
-    init = (data[13] - data[3]) / 10
-    for i in range(0, len(data) - 10):
-        slope = (data[i+5] - data[i]) / 5
-        if abs(slope) < abs(init) * 0.2:
-            if i < int(0.7 * len(data)):
-                continue
+    offset = 15
+    init = (data[offset + 1] - data[1]) / offset
+    for i in range(1, len(data) - offset):
+        slope = (data[i+offset] - data[i]) / offset
+        print(abs(slope), abs(init))
+        if abs(slope) <= abs(init) * 0.15:
+            # if i < int(0.7 * len(data)):
+            #     continue
             return i
     return None
 
@@ -102,7 +120,7 @@ ERROR_INT = 40
 # EXERCISE A : Calculations
 ###############################################################################
 
-def exercise_a_calculations():
+def get_a_steady_states():
     xls = pd.read_excel('./C5_HT36C_A.xls')
     # print(xls.columns)
     result = [[[] for _ in range(10)] for _ in range(4)]
@@ -117,9 +135,210 @@ def exercise_a_calculations():
         for j in range(10):
             ss_result[i].append(find_steady_state(result[i][j]))
     steady_states = [80, 30, 60, 60]
-    
-    
 
+def exercise_a_calculations():
+    xls = pd.read_excel('./Tables/Table 1.xlsx')
+    print(xls.columns)
+    a = np.array
+    d_i = 0.0089
+    L = 0.66*a([4, 3, 2, 1])
+    A_i = d_i*np.pi*L
+    T_h = 273.15 + 47.5
+    T_c = 273.15 + 12.7
+    c_p_c = 4182
+    c_p_h = 4180
+    rho_h = 1/(1.0114*10**-3)
+    rho_c = 1/(1.0008*10**-3)
+    m_dot_h = a(xls["V_hot [l/min]"])*rho_h/60000
+    m_dot_c = a(xls["V_cold [l/min]"])*rho_c/60000
+    T_c_i = a([12.8, 12.5, 12.6, 12.7])
+    T_h_i = a(xls['T1 [°C]'])
+    T_c_o = a(xls['T10 [°C]'])
+    T_h_o = a([38.7, 41.1, 42.8, 44.8])
+    q_c = -1*m_dot_c*c_p_c*(T_c_i - T_c_o)
+    q_h = m_dot_h*c_p_h*(T_h_i - T_h_o)
+    epsilon = q_h/(np.minimum(m_dot_c*c_p_c, m_dot_h*c_p_h)*(T_h_i - T_c_i))
+    delta_T1 = T_h_i - T_c_o
+    delta_T2 = T_h_o - T_c_i
+    lmtd = (delta_T1 - delta_T2)/np.log(delta_T1/delta_T2)
+    U_measured = q_h/(A_i*lmtd)
+    delta_Delta_T = np.sqrt(0.1**2*2)
+    delta_m_dot = 0.01*rho_h/60000
+    delta_Q = np.sqrt((delta_m_dot*c_p_h*(T_h_i - T_h_o))**2+
+                      (m_dot_h*c_p_h*delta_Delta_T)**2)
+    delta_lmtd = np.sqrt(((delta_T1)*np.log(delta_T1/delta_T2) + delta_T2 - delta_T1)*delta_Delta_T/(delta_T1*np.log(delta_T1/delta_T2)**2)**2 +
+                         ((-delta_T2)*np.log(delta_T1/delta_T2) + delta_T1 - delta_T2)*delta_Delta_T/(delta_T2*np.log(delta_T1/delta_T2)**2)**2)
+    delta_U_measured = np.sqrt((delta_Q/(A_i*lmtd))**2 + (-q_h*delta_lmtd/(A_i*lmtd**2))**2)
+    mu = 567*10**-6
+    Re = 4*m_dot_h/(np.pi*d_i*mu)
+    k = 641*10**-3
+    Pr = 0.894
+    Nu = 0.023*Re**0.8*Pr**0.3
+    h_i = k*Nu/d_i
+    d_o = 0.014
+    A_o = d_o*np.pi*L
+    Re = 4*m_dot_h/(np.pi*d_o*mu)
+    Nu = 0.023*Re**0.8*Pr**0.3
+    h_o = k*Nu/d_o
+    k_steel = 15.2
+    R_tube = np.log(d_o/d_i)/(2*np.pi*L*k_steel)
+    U_predicted = (1/(h_i*A_i) + R_tube + 1/(h_o*A_o))**-1/A_i
+    U_diff = abs(U_measured - U_predicted)/(U_predicted)*100
+    
+    save(m_dot_h, m_dot_c, T_c_i, T_h_i, T_c_o, T_h_o, q_c, q_h, epsilon, 
+         lmtd, U_measured, delta_U_measured, h_i, h_o, R_tube, U_predicted, U_diff)
+    print_values(2)
+    VALUES.clear()
+    save(rho_c, rho_h, c_p_c, c_p_h, mu, Pr, k, k_steel, length=4)
+    print_values(3)
+
+def get_b_steady_states():
+    xls = pd.read_excel('./C5_HT36C_B1.xls')
+    # print(xls.columns)
+    result = [[[] for _ in range(10)] for _ in range(4)]
+    tube_no = xls['Number of Tubes']
+    for i in range(len(xls['Number of Tubes'])):
+        if np.isnan(tube_no[i]): continue
+        for j in range(10):
+            column = f'Temp T{j+1} [∞C]'
+            result[int(tube_no[i]) - 1][j].append(xls[column][i])
+    ss_result = [[] for _ in range(4)]
+    for i in range(4):
+        for j in range(10):
+            ss_result[i].append(find_steady_state(result[i][j]))
+    steady_states = [80, 30, 60, 60]
+
+def exercise_b_calculations():
+    xls = pd.read_excel('./Tables/Table 1.xlsx')
+    print(xls.columns)
+    a = np.array
+    d_i = 0.0089
+    L = 0.66*4
+    A_i = d_i*np.pi*L
+    T_h = 273.15 + 47.5
+    T_c = 273.15 + 12.7
+    c_p_c = 4182
+    c_p_h = 4180
+    rho_h = 1/(1.0114*10**-3)
+    rho_c = 1/(1.0008*10**-3)
+    m_dot_h = a(xls["V_hot [l/min]"])*rho_h/60000
+    m_dot_c = a(xls["V_cold [l/min]"])*rho_c/60000
+    T_c_i = a([12.8, 12.5, 12.6, 12.7])
+    T_h_i = a(xls['T1 [°C]'])
+    T_c_o = a(xls['T10 [°C]'])
+    T_h_o = a([38.7, 41.1, 42.8, 44.8])
+    q_c = -1*m_dot_c*c_p_c*(T_c_i - T_c_o)
+    q_h = m_dot_h*c_p_h*(T_h_i - T_h_o)
+    epsilon = q_h/(np.minimum(m_dot_c*c_p_c, m_dot_h*c_p_h)*(T_h_i - T_c_i))
+    delta_T1 = T_h_i - T_c_i
+    delta_T2 = T_h_o - T_c_o
+    lmtd = (delta_T1 - delta_T2)/np.log(delta_T1/delta_T2)
+    U_measured = q_h/(A_i*lmtd)
+    delta_Delta_T = np.sqrt(0.1**2*2)
+    delta_m_dot = 0.01*rho_h/60000
+    delta_Q = np.sqrt((delta_m_dot*c_p_h*(T_h_i - T_h_o))**2+
+                      (m_dot_h*c_p_h*delta_Delta_T)**2)
+    delta_lmtd = np.sqrt(((delta_T1)*np.log(delta_T1/delta_T2) + delta_T2 - delta_T1)*delta_Delta_T/(delta_T1*np.log(delta_T1/delta_T2)**2)**2 +
+                         ((-delta_T2)*np.log(delta_T1/delta_T2) + delta_T1 - delta_T2)*delta_Delta_T/(delta_T2*np.log(delta_T1/delta_T2)**2)**2)
+    delta_U_measured = np.sqrt((delta_Q/(A_i*lmtd))**2 + (-q_h*delta_lmtd/(A_i*lmtd**2))**2)
+    
+    save(m_dot_h, m_dot_c, T_c_i, T_h_i, T_c_o, T_h_o, q_c, q_h, epsilon, 
+         lmtd, U_measured, delta_U_measured)
+    print_values(5)
+
+def get_c_steady_states():
+    xls = pd.read_excel('./C5_HT36C_C1.xls')
+    ss_result = []
+    for j in range(10):
+        ss_result.append(find_steady_state(xls[f"Temp T{j+1} [∞C]"]))
+    print(ss_result)
+    # ss1 = 35
+    # ss2 = 20
+
+get_c_steady_states()
+
+def exercise_c_calculations():
+    a = np.array
+    d_i = 0.0089
+    L = 0.66*4
+    A_i = d_i*np.pi*L
+    c_p_c = 4182
+    c_p_h = 4180
+    rho_h = 1/(1.01*10**-3)
+    rho_c = 1/(1.0002*10**-3)
+    m_dot_h = a([1.01, 0.48])*rho_h/60000
+    m_dot_c = a([1.01, 0.97])*rho_c/60000
+    T_c_i = a([13, 13.6])
+    T_h_i = a([43.6, 44.5])
+    T_c_o = a([26.8, 22.8])
+    T_h_o = a([30.9, 29.1])
+    q_c = -1*m_dot_c*c_p_c*(T_c_i - T_c_o)
+    q_h = m_dot_h*c_p_h*(T_h_i - T_h_o)
+    epsilon = q_h/(np.minimum(m_dot_c*c_p_c, m_dot_h*c_p_h)*(T_h_i - T_c_i))
+    delta_T1 = T_h_i - T_c_i
+    delta_T2 = T_h_o - T_c_o
+    lmtd = (delta_T1 - delta_T2)/np.log(delta_T1/delta_T2)
+    U_measured = q_h/(A_i*lmtd)
+    delta_Delta_T = np.sqrt(0.1**2*2)
+    delta_m_dot = 0.01*rho_h/60000
+    delta_Q = np.sqrt((delta_m_dot*c_p_h*(T_h_i - T_h_o))**2+
+                      (m_dot_h*c_p_h*delta_Delta_T)**2)
+    delta_lmtd = np.sqrt(((delta_T1)*np.log(delta_T1/delta_T2) + delta_T2 - delta_T1)*delta_Delta_T/(delta_T1*np.log(delta_T1/delta_T2)**2)**2 +
+                         ((-delta_T2)*np.log(delta_T1/delta_T2) + delta_T1 - delta_T2)*delta_Delta_T/(delta_T2*np.log(delta_T1/delta_T2)**2)**2)
+    delta_U_measured = np.sqrt((delta_Q/(A_i*lmtd))**2 + (-q_h*delta_lmtd/(A_i*lmtd**2))**2)
+    
+    save(m_dot_h, m_dot_c, T_c_i, T_h_i, T_c_o, T_h_o, q_c, q_h, epsilon, 
+         lmtd, U_measured, delta_U_measured)
+    print_values(7)
+
+def get_d_steady_states():
+    xls = pd.read_excel('./C5_HT36C_D4.xls')
+    ss_result = []
+    for j in range(10):
+        ss_result.append(find_steady_state(xls[f"Temp T{j+1} [∞C]"]))
+    print(ss_result)
+    # ss1 = 37
+    # ss2 = 29
+    # ss3 = 28
+
+get_d_steady_states()
+
+def exercise_d_calculations():
+    xls = pd.read_excel('./Tables/Table 8.xlsx')
+    a = np.array
+    d_i = 0.0089
+    L = 0.66*4
+    A_i = d_i*np.pi*L
+    c_p_c = 4182
+    c_p_h = 4180
+    rho_h = 1/(1.012*10**-3)
+    rho_c = 1/(1.0002*10**-3)
+    m_dot_h = a([3, 2, 1, 0.51])*rho_h/60000
+    m_dot_c = a([0.95, 0.94, 1.07, 1.1])*rho_c/60000
+    T_c_i = a(xls['T6 [°C]'])
+    T_h_i = a(xls['T1 [°C]'])
+    T_c_o = a(xls['T10 [°C]'])
+    T_h_o = a(xls['T5 [°C]'])
+    q_c = -m_dot_c*c_p_c*(T_c_i - T_c_o)
+    q_h = m_dot_h*c_p_h*(T_h_i - T_h_o)
+    epsilon = q_h/(np.minimum(m_dot_c*c_p_c, m_dot_h*c_p_h)*(T_h_i - T_c_i))
+    delta_T1 = T_h_i - T_c_i
+    delta_T2 = T_h_o - T_c_o
+    lmtd = (delta_T1 - delta_T2)/np.log(delta_T1/delta_T2)
+    U_measured = q_h/(A_i*lmtd)
+    delta_Delta_T = np.sqrt(0.1**2*2)
+    delta_m_dot = 0.01*rho_h/60000
+    delta_Q = np.sqrt((delta_m_dot*c_p_h*(T_h_i - T_h_o))**2+
+                      (m_dot_h*c_p_h*delta_Delta_T)**2)
+    delta_lmtd = np.sqrt(((delta_T1)*np.log(delta_T1/delta_T2) + delta_T2 - delta_T1)*delta_Delta_T/(delta_T1*np.log(delta_T1/delta_T2)**2)**2 +
+                         ((-delta_T2)*np.log(delta_T1/delta_T2) + delta_T1 - delta_T2)*delta_Delta_T/(delta_T2*np.log(delta_T1/delta_T2)**2)**2)
+    delta_U_measured = np.sqrt((delta_Q/(A_i*lmtd))**2 + (-q_h*delta_lmtd/(A_i*lmtd**2))**2)
+
+    
+    save(m_dot_h, m_dot_c, T_c_i, T_h_i, T_c_o, T_h_o, q_c, q_h, epsilon, 
+         lmtd, U_measured, delta_U_measured)
+    print_values(9)
+    
 def figure_1_plot():
 
     xls = pd.read_excel('./Data/C5_HTFin.xlsx')
@@ -361,8 +580,6 @@ def best_fit_parametric_est():
         result.append(h_min)
     return result
 
-print(best_fit_parametric_est())
-
 def table_2_calculations():
 
     t1_columns = ['T_f [C]', 'U_air [m/s]', 'Re_D', 'Gr_D', 'Pr', 'Nu_D', 
@@ -412,6 +629,12 @@ def table_2_calculations():
     
 def calculations():
     exercise_a_calculations()
+    VALUES.clear()
+    exercise_b_calculations()
+    VALUES.clear()
+    exercise_c_calculations()
+    VALUES.clear()
+    exercise_d_calculations()
     # figure_1_plot()
     return
 
